0ac6402f703512215d19489c91d95c4d92d2fb56
anamoraisdev <anamorais.dev@gmail.com>
2022-11-04 17:20:14 -0300

    Função de gerar numeros aleatorios

--- game.py
+++ game.py
@@ -1,12 +1,6 @@
 import random 
 import time 
 score = 0
-numero1 = random.randint(50,200)
-numero2 = random.randint(201,350)
-numero3 = random.randint(351,500)
-numero4 = random.randint(501,650)
-resposta1 = numero1 + numero2
-resposta2 = numero3 + numero4
 
 print("Bem-vindo ao Quiz Matemático")
 user = input ("Vamos começar? (sim/não):")
@@ -25,6 +19,9 @@ def countdown(t):
     print('VALENDO!!') 
 t = (5) 
 countdown(int(t)) 
+
+def gerar_numero:
+    return random.randint(50,500)
 
 
 
