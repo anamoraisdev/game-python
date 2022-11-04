de7eabc70147b4b31b6189e10ac1f4f8047abe71
anamoraisdev <anamorais.dev@gmail.com>
2022-11-04 16:03:41 -0300

    Adicionando interação com usuario(perguntas)

--- game.py
+++ game.py
@@ -8,6 +8,14 @@ numero4 = random.randint(501,650)
 resposta1 = numero1 + numero2
 resposta2 = numero3 + numero4
 
+print("Bem-vindo ao Quiz Matemático")
+user = input ("Vamos começar? (sim/não):")
+if user == "sim":
+    print("Começando em ...")
+if user == "não":
+    quit()
+
+
 
 
 
