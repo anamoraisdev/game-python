89ca8b89978ddfeddec48d99cb7e0688494b6d64
anamoraisdev <anamorais.dev@gmail.com>
2022-11-04 15:58:09 -0300

    impotando time, random e criando variaveis das somas

---
+++ game.py
@@ -0,0 +1,19 @@
+import random 
+import time 
+score = 0
+numero1 = random.randint(50,200)
+numero2 = random.randint(201,350)
+numero3 = random.randint(351,500)
+numero4 = random.randint(501,650)
+resposta1 = numero1 + numero2
+resposta2 = numero3 + numero4
+
+
+
+
+
+
+
+
+
+
